1e61c482793fb9cd5ca43197271e30cc892bb3df
James Johnson <james.johnson@tiny.cloud>
2020-03-11 16:30:16 +1000

    TINY-4763: Added basic test

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: './src/test/.+Test\\.ts$',
+  collectCoverage: false,
+  collectCoverageFrom: ['src/main/**/*.{js,jsx,ts,tsx}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageReporters: ['text-summary', 'lcov'],
+};
--- src/test/rules/RuleNoDirectImportsTest.ts
+++ src/test/rules/RuleNoDirectImportsTest.ts
@@ -1,23 +1,26 @@
 import { RuleTester } from "eslint";
 import { noDirectImports } from "../../main/ts/rules/RuleNoDirectImports";
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parser: require.resolve("@typescript-eslint/parser"),
+  parserOptions: { sourceType: 'module' }
+});
 
 ruleTester.run("no-direct-imports", noDirectImports, {
   valid: [
-      {
-          code: "import { Fun } from '@ephox/katmari';"
-      }
+    {
+      code: "import { Fun } from '@ephox/katmari';"
+    }
   ],
 
   invalid: [
-      {
-          code: "import { Arr } from '@ephox/katmari/lib/main/api/Arr';",
-          errors: [{ message: "Direct import to @ephox/katmari/lib/main/api/Arr is forbidden." }]
-      },
-      {
-          code: "import { Unicode } from '@ephox/katamari/src/main/ts/ephox/katamari/api/Unicode';",
-          errors: [{ message: "Direct import to @ephox/katamari/src/main/ts/ephox/katamari/api/Unicode is forbidden." }]
-      }
+    {
+      code: "import { Arr } from '@ephox/katmari/lib/main/api/Arr';",
+      errors: [{ message: "Direct import to @ephox/katmari/lib/main/api/Arr is forbidden." }]
+    },
+    {
+      code: "import { Unicode } from '@ephox/katamari/src/main/ts/ephox/katamari/api/Unicode';",
+      errors: [{ message: "Direct import to @ephox/katamari/src/main/ts/ephox/katamari/api/Unicode is forbidden." }]
+    }
   ]
 });
