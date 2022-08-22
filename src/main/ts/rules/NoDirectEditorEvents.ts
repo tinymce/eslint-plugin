8f6af62c5a401b44fc8dc6cf4a1127707494d7b1
Lee Newson <lee.newson@tiny.cloud>
2022-08-22 12:55:26 +1000

    TINY-9003: Added new `no-direct-editor-events` rule to ensure events are only dispatched inside `api/Events.ts`

---
+++ src/main/ts/rules/NoDirectEditorEvents.ts
@@ -0,0 +1,39 @@
+import { Rule } from 'eslint';
+import { isEditorMemberExpression } from '../utils/EditorUtils';
+import { extractMemberIdentifiers } from '../utils/ExtractUtils';
+import { isPathInDemo, isPathInTest } from '../utils/PathUtils';
+
+export const noDirectEditorEvents: Rule.RuleModule = {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'Disallows editor events from being dispatched outside api/Events.ts.'
+    },
+    messages: {
+      noDirectEditorEvents: 'Dispatching events is forbidden outside api/Events.ts.',
+    }
+  },
+  create: (context) => {
+    const filename = context.getFilename();
+    // Ignore in tests, demos or Events.ts
+    // NOTE: To allow for some legacy setups we currently only enforce `Events.ts` instead of `api/Events.ts`
+    if (isPathInTest(filename) || isPathInDemo(filename) || filename.endsWith('/Events.ts')) {
+      return {};
+    } else {
+      return {
+        CallExpression: (node) => {
+          const callee = node.callee;
+          if (callee.type === 'MemberExpression' && isEditorMemberExpression(callee)) {
+            const identifiers = extractMemberIdentifiers(callee);
+            if (identifiers[1]?.name === 'dispatch' || identifiers[1]?.name === 'fire') {
+              context.report({
+                node,
+                messageId: 'noDirectEditorEvents'
+              });
+            }
+          }
+        }
+      };
+    }
+  }
+};
