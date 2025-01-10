ef1e365309e9dceea1d199d5bae6dd4cbceac280
ltrouton <luke.trouton@tiny.cloud>
2025-01-10 16:10:30 +1000

    TINY-11728: Clean up already defined typescript eslint rules. Add typescript stylistic rules

--- src/main/ts/configs/Standard.ts
+++ src/main/ts/configs/Standard.ts
@@ -5,8 +5,7 @@ export const base: Linter.Config = {
     // TODO: Look at enabling recommended eslint options
     // 'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
-    // TODO: Look at enabling recommended typescript stylistic options
-    // 'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
   ],
   parser: '@typescript-eslint/parser',
   plugins: [
@@ -17,23 +16,38 @@ export const base: Linter.Config = {
     'prefer-arrow',
   ],
   rules: {
-    '@typescript-eslint/await-thenable': 'error',
-    '@typescript-eslint/ban-ts-comment': 'error',
-    '@typescript-eslint/consistent-type-assertions': 'error',
-    '@typescript-eslint/consistent-type-definitions': 'error',
-    '@typescript-eslint/explicit-member-accessibility': [ 'error', { accessibility: 'explicit' }],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      { accessibility: 'explicit' },
+    ],
     '@typescript-eslint/member-ordering': 'error',
-    '@typescript-eslint/naming-convention': [ 'error', { selector: 'typeLike', format: [ 'PascalCase' ] }],
-    '@typescript-eslint/no-for-in-array': 'error',
-    '@typescript-eslint/no-implied-eval': 'error',
-    '@typescript-eslint/no-inferrable-types': [ 'error', { ignoreParameters: true, ignoreProperties: true }],
-    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: 'typeLike', format: [ 'PascalCase' ] },
+    ],
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      { ignoreParameters: true, ignoreProperties: true },
+    ],
+    'no-shadow': 'off',
     '@typescript-eslint/no-shadow': [ 'error', { hoist: 'all' }],
-    '@typescript-eslint/no-unused-expressions': [ 'error', { allowTernary: true }],
-    '@typescript-eslint/no-unused-vars': [ 'warn', { vars: 'all', args: 'after-used', argsIgnorePattern: '^_', caughtErrors: 'all', caughtErrorsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_', ignoreRestSiblings: true, }],
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      { allowTernary: true },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
     '@typescript-eslint/parameter-properties': 'error',
-    '@typescript-eslint/prefer-for-of': 'error',
-    '@typescript-eslint/prefer-function-type': 'error',
     '@typescript-eslint/unified-signatures': 'error',
 
     '@typescript-eslint/array-type': 'off',
@@ -61,6 +75,8 @@ export const base: Linter.Config = {
     '@typescript-eslint/prefer-as-const': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off', // Needs StrictNullChecks
     '@typescript-eslint/switch-exhaustiveness-check': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/non-nullable-type-assertion-style': 'off',
 
     'arrow-body-style': 'error',
     'constructor-super': 'error',
@@ -94,7 +110,7 @@ export const base: Linter.Config = {
 
     'complexity': 'off',
     'no-invalid-this': 'off',
-    'valid-typeof': 'off',  // Disabled as it's handled by TypeScript
+    'valid-typeof': 'off', // Disabled as it's handled by TypeScript
 
     'import/no-duplicates': 'error',
     'import/order': 'off',
@@ -105,7 +121,11 @@ export const base: Linter.Config = {
     'mocha/no-exclusive-tests': 'error',
     'mocha/no-identical-title': 'error',
 
-    '@stylistic/array-bracket-spacing': [ 'error', 'always', { objectsInArrays: false, arraysInArrays: false }],
+    '@stylistic/array-bracket-spacing': [
+      'error',
+      'always',
+      { objectsInArrays: false, arraysInArrays: false },
+    ],
     '@stylistic/arrow-parens': [ 'error', 'always' ],
     '@stylistic/arrow-spacing': 'error',
     '@stylistic/brace-style': 'error',
@@ -113,23 +133,47 @@ export const base: Linter.Config = {
     '@stylistic/computed-property-spacing': 'error',
     '@stylistic/dot-location': [ 'error', 'property' ],
     '@stylistic/func-call-spacing': 'error',
-    '@stylistic/indent': [ 'error', 2, { FunctionDeclaration: { parameters: 'first' }, FunctionExpression: { parameters: 'first' }, SwitchCase: 1 }],
-    '@stylistic/key-spacing': [ 'error', { beforeColon: false, afterColon: true, mode: 'strict' }],
+    '@stylistic/indent': [
+      'error',
+      2,
+      {
+        FunctionDeclaration: { parameters: 'first' },
+        FunctionExpression: { parameters: 'first' },
+        SwitchCase: 1,
+      },
+    ],
+    '@stylistic/key-spacing': [
+      'error',
+      { beforeColon: false, afterColon: true, mode: 'strict' },
+    ],
     '@stylistic/keyword-spacing': 'error',
-    '@stylistic/member-delimiter-style': [ 'error', { multiline: { delimiter: 'semi', requireLast: true },singleline: { delimiter: 'semi', requireLast: false }}],
+    '@stylistic/member-delimiter-style': [
+      'error',
+      {
+        multiline: { delimiter: 'semi', requireLast: true },
+        singleline: { delimiter: 'semi', requireLast: false },
+      },
+    ],
     '@stylistic/new-parens': 'error',
     '@stylistic/no-multi-spaces': [ 'error', { ignoreEOLComments: true }],
     '@stylistic/no-multiple-empty-lines': [ 'error', { max: 1 }],
     '@stylistic/no-trailing-spaces': 'error',
     '@stylistic/no-whitespace-before-property': 'error',
-    '@stylistic/object-curly-spacing': [ 'error', 'always', { objectsInObjects: false }],
+    '@stylistic/object-curly-spacing': [
+      'error',
+      'always',
+      { objectsInObjects: false },
+    ],
     '@stylistic/quote-props': [ 'error', 'consistent-as-needed' ],
     '@stylistic/quotes': [ 'error', 'single', { allowTemplateLiterals: true }],
     '@stylistic/rest-spread-spacing': 'error',
     '@stylistic/semi-spacing': 'error',
     '@stylistic/semi': [ 'error', 'always' ],
     '@stylistic/space-before-blocks': 'error',
-    '@stylistic/space-before-function-paren': [ 'error', { anonymous: 'always', named: 'never' }],
+    '@stylistic/space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never' },
+    ],
     '@stylistic/space-infix-ops': 'error',
     '@stylistic/space-unary-ops': 'error',
     '@stylistic/spaced-comment': 'error',
@@ -142,7 +186,7 @@ export const base: Linter.Config = {
     // Continue using default max-len option to avoid changing too many files that
     // have max-len ignore comments
     '@stylistic/max-len': 'off',
-  }
+  },
 };
 
 export const standard: Linter.Config = {
@@ -165,5 +209,5 @@ export const standard: Linter.Config = {
     '@tinymce/prefer-fun': 'error',
     '@tinymce/prefer-mcagar-tiny-assertions': 'off',
     '@tinymce/prefer-mcagar-tiny-dom': 'off',
-  }
+  },
 };
